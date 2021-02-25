9f52d3bb72c54e6e78f62bb01e1e9a8ea920ce63
root <root@grid39.dwslgrid.ece.drexel.edu>
2021-02-25 22:26:47 +0000

    more Python3 updates

--- timeseries_slicer.py
+++ timeseries_slicer.py
@@ -3,7 +3,7 @@ import analyze_stats
 import matplotlib.pyplot as plt
 
 def slice_timeseries(x, l=128, d=64, max_k = None):
-    k = (len(x) - l + 1) / d
+    k = (len(x) - l + 1) // d
     if not max_k == None:
         k = min(k, max_k)
     X = np.zeros([k,2,l], dtype=np.float32)
@@ -12,7 +12,7 @@ def slice_timeseries(x, l=128, d=64, max_k = None):
         w = np.ones([l])
         # Sin Window
         #w = np.sin(np.arange(0,np.pi,np.pi/l))
-        x_i =      x[i*d:i*d+l] * w
+        x_i = x[i*d:i*d+l] * w
         X[i,0,:] = np.real(x_i)
         X[i,1,:] = np.imag(x_i)
         energy = analyze_stats.calc_vec_energy(X[i])
@@ -27,18 +27,17 @@ def slice_timeseries_dict(td, l=128, d=64, max_k = None):
     return nd
 
 def slice_timeseries_real(x, l=128, d=64, max_k = None):
-    k = (len(x) - l + 1) / d
+    k = (len(x) - l + 1) // d
     if not max_k == None:
         k = max(k, max_k)
     X = np.zeros([k,1,l], dtype=np.float32)
     for i in range(0,k):
-        x_i =      x[i*d:i*d+l]
+        x_i = x[i*d:i*d+l]
         X[i,0,:] = x_i
     return X
-
 
 def slice_timeseries_real_dict(td, l=128, d=64, max_k = None):
     nd = {}
     for k,v in td.items():
-        nd[int(k)] = slice_timeseries_real(v,l,d,max_k)
+        nd[k] = slice_timeseries_real(v,l,d,max_k)
     return nd
