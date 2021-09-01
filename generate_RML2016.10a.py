b537c641dd1a33a270909e73a20a0c50de832afb
edward-reehorst <reehorst.3@osu.edu>
2021-09-01 08:04:18 -0400

    remove dependency on gr-mapper, there is a problem with the analog modulation so code is set up for digital signals only

--- generate_RML2016.10a.py
+++ generate_RML2016.10a.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 from transmitters import transmitters
 from source_alphabet import source_alphabet
-import analyze_stats
+# import analyze_stats
 from gnuradio import channels, gr, blocks
 import numpy as np
-import numpy.fft, pickle, gzip
+import pickle
 import random
 
 '''
@@ -12,6 +12,7 @@ Generate dataset with dynamic channel model across range of SNRs
 '''
 
 apply_channel = True
+thermal_noise = False
 
 dataset = {}
 
@@ -19,64 +20,76 @@ dataset = {}
 # {('mod type', SNR): np.array(nvecs_per_key, 2, vec_length), etc}
 
 # CIFAR-10 has 6000 samples/class. CIFAR-100 has 600. Somewhere in there seems like right order of magnitude
-nvecs_per_key = 1000
-vec_length = 128
-snr_vals = range(-20,20,2)
+nvecs_per_key = 1_000 if thermal_noise and apply_channel else 20_000
+vec_length = 1024
+if thermal_noise:
+    snr_vals = range(-20,20,2)
+else:
+    snr_vals = ['clean']
 
 for snr in snr_vals:
-    nvecs_per_key = 1000
-    vec_length = 128
     print("snr is ", snr)
     for alphabet_type in transmitters.keys():
         for i,mod_type in enumerate(transmitters[alphabet_type]):
-          dataset[(mod_type.modname, snr)] = np.zeros([nvecs_per_key, 2, vec_length], dtype=np.float32)
-          # moar vectors!
-          insufficient_modsnr_vectors = True
-          modvec_indx = 0
-          while insufficient_modsnr_vectors:
-              tx_len = int(10e3)
-              if mod_type.modname == "QAM16":
-                  tx_len = int(20e3)
-              if mod_type.modname == "QAM64":
-                  tx_len = int(30e3)
-              src = source_alphabet(alphabet_type, tx_len, True)
-              mod = mod_type()
-              fD = 1
-              delays = [0.0, 0.9, 1.7]
-              mags = [1, 0.8, 0.3]
-              ntaps = 8
-              noise_amp = 10**(-snr/10.0)
-              chan = channels.dynamic_channel_model( 200e3, 0.01, 50, .01, 0.5e3, 8, fD, True, 4, delays, mags, ntaps, noise_amp, 0x1337 )
+            print(mod_type.modname)
+            dataset[(mod_type.modname, snr)] = np.zeros([nvecs_per_key, 2, vec_length], dtype=np.float32)
+            # moar vectors!
+            insufficient_modsnr_vectors = True
+            modvec_indx = 0
+            while insufficient_modsnr_vectors:
+                tx_len = int(10e3)
+                if mod_type.modname == "QAM16":
+                    tx_len = int(20e3)
+                if mod_type.modname == "QAM64":
+                    tx_len = int(30e3)
+                src = source_alphabet(alphabet_type, tx_len, True)
+                mod = mod_type()
+                fD = 1
+                delays = [0.0, 0.9, 1.7]
+                mags = [1, 0.8, 0.3]
+                ntaps = 8
+                if thermal_noise:
+                    noise_amp = 10**(-snr/10.0)
+                else:
+                    noise_amp = 0.0
+                chan = channels.dynamic_channel_model( 200e3, 0.01, 50, .01, 0.5e3, 8, fD, True, 4, delays, mags, ntaps, noise_amp, 0x1337 )
 
-              snk = blocks.vector_sink_c()
+                snk = blocks.vector_sink_c()
 
-              tb = gr.top_block()
+                tb = gr.top_block()
 
-              # connect blocks
-              if apply_channel:
-                  tb.connect(src, mod, chan, snk)
-              else:
-                  tb.connect(src, mod, snk)
-              tb.run()
+                # connect blocks
+                if apply_channel:
+                    tb.connect(src, mod, chan, snk)
+                else:
+                    tb.connect(src, mod, snk)
+                tb.run()
 
-              raw_output_vector = np.array(snk.data(), dtype=np.complex64)
-              # start the sampler some random time after channel model transients (arbitrary values here)
-              sampler_indx = random.randint(50, 500)
-              while sampler_indx + vec_length < len(raw_output_vector) and modvec_indx < nvecs_per_key:
-                  sampled_vector = raw_output_vector[sampler_indx:sampler_indx+vec_length]
-                  # Normalize the energy in this vector to be 1
-                  energy = np.sum((np.abs(sampled_vector)))
-                  sampled_vector = sampled_vector / energy
-                  dataset[(mod_type.modname, snr)][modvec_indx,0,:] = np.real(sampled_vector)
-                  dataset[(mod_type.modname, snr)][modvec_indx,1,:] = np.imag(sampled_vector)
-                  # bound the upper end very high so it's likely we get multiple passes through
-                  # independent channels
-                  sampler_indx += random.randint(vec_length, round(len(raw_output_vector)*.05))
-                  modvec_indx += 1
+                raw_output_vector = np.array(snk.data(), dtype=np.complex64)
+                # start the sampler some random time after channel model transients (arbitrary values here)
+                sampler_indx = random.randint(50, 500)
+                while sampler_indx + vec_length < len(raw_output_vector) and modvec_indx < nvecs_per_key:
+                    sampled_vector = raw_output_vector[sampler_indx:sampler_indx+vec_length]
+                    # Normalize the energy in this vector to be 1
+                    energy = np.sum((np.abs(sampled_vector)))
+                    sampled_vector = sampled_vector / energy
+                    dataset[(mod_type.modname, snr)][modvec_indx,0,:] = np.real(sampled_vector)
+                    dataset[(mod_type.modname, snr)][modvec_indx,1,:] = np.imag(sampled_vector)
+                    # bound the upper end very high so it's likely we get multiple passes through
+                    # independent channels
+                    sampler_indx += random.randint(vec_length, round(len(raw_output_vector)*.05))
+                    modvec_indx += 1
 
-              if modvec_indx == nvecs_per_key:
-                  # we're all done
-                  insufficient_modsnr_vectors = False
+                if modvec_indx == nvecs_per_key:
+                    # we're all done
+                    insufficient_modsnr_vectors = False
 
 print("all done. writing to disk")
-pickle.dump(dataset, open("datasets/RML2016.10a_dict.dat", "wb"))
+if apply_channel:
+    if thermal_noise:
+        save_name = "datasets/RML2016.10a_dict.dat"
+    else:
+        save_name = "datasets/RML2016.10a_clean_dict.dat"
+else:
+    save_name = "datasets/RML2016.10a_debug_dict.dat"
+pickle.dump(dataset, open(save_name, "wb"))
--- transmitters.py
+++ transmitters.py
@@ -3,73 +3,131 @@ import time, math
 from scipy.signal import get_window
 from gnuradio import gr, blocks, digital, analog, filter
 from gnuradio.filter import firdes
-import sys
-sys.path.append("/usr/local/lib/python3/dist-packages")
-import mapper
+
 sps = 8
 ebw = 0.35
 
-class transmitter_mapper(gr.hier_block2):
-    def __init__(self, modtype, symvals, txname, samples_per_symbol=2, excess_bw=0.35):
-        gr.hier_block2.__init__(self, txname,
+# class transmitter_mapper(gr.hier_block2):
+    # def __init__(self, modtype, m, txname, samples_per_symbol=2, excess_bw=0.35):
+        # gr.hier_block2.__init__(self, txname,
+            # gr.io_signature(1, 1, gr.sizeof_char),
+            # gr.io_signature(1, 1, gr.sizeof_gr_complex))
+        # self.mod = mapper.mapper(modtype, symvals)
+        # # pulse shaping filter
+        # nfilts = 32
+        # ntaps = nfilts * 11 * int(samples_per_symbol)    # make nfilts filters of ntaps each
+        # rrc_taps = filter.firdes.root_raised_cosine(
+            # nfilts,          # gain
+            # nfilts,          # sampling rate based on 32 filters in resampler
+            # 1.0,             # symbol rate
+            # excess_bw, # excess bandwidth (roll-off factor)
+            # ntaps)
+        # self.rrc_filter = filter.pfb_arb_resampler_ccf(samples_per_symbol, rrc_taps)
+        # self.connect(self, self.mod, self.rrc_filter, self)
+        # self.rate = const.bits_per_symbol()
+
+# class transmitter_bpsk(transmitter_mapper):
+    # modname = "BPSK"
+    # def __init__(self):
+        # transmitter_mapper.__init__(self, mapper.BPSK,
+            # [0,1], "transmitter_bpsk", sps, ebw)
+
+class transmitter_mpsk(gr.hier_block2):
+    def __init__(self, m, modname):
+        self.modname = modname
+        constellation = digital.psk_constellation(m)
+        gr.hier_block2.__init__(self, "transmitter_" + modname,
             gr.io_signature(1, 1, gr.sizeof_char),
             gr.io_signature(1, 1, gr.sizeof_gr_complex))
-        self.mod = mapper.mapper(modtype, symvals)
-        # pulse shaping filter
-        nfilts = 32
-        ntaps = nfilts * 11 * int(samples_per_symbol)    # make nfilts filters of ntaps each
-        rrc_taps = filter.firdes.root_raised_cosine(
-            nfilts,          # gain
-            nfilts,          # sampling rate based on 32 filters in resampler
-            1.0,             # symbol rate
-            excess_bw, # excess bandwidth (roll-off factor)
-            ntaps)
-        self.rrc_filter = filter.pfb_arb_resampler_ccf(samples_per_symbol, rrc_taps)
-        self.connect(self, self.mod, self.rrc_filter, self)
-        #self.rate = const.bits_per_symbol()
+        self.repack = blocks.unpacked_to_packed_bb(1, gr.GR_MSB_FIRST)
+        self.mod = digital.generic_mod(constellation, samples_per_symbol=sps, excess_bw=ebw)
+        self.connect( self, self.repack, self.mod, self )
 
-class transmitter_bpsk(transmitter_mapper):
+
+class transmitter_mqam(gr.hier_block2):
+    def __init__(self, m, modname):
+        self.modname = modname
+        constellation = digital.qam_constellation(m)
+        gr.hier_block2.__init__(self, "transmitter_" + modname,
+            gr.io_signature(1, 1, gr.sizeof_char),
+            gr.io_signature(1, 1, gr.sizeof_gr_complex))
+        self.repack = blocks.unpacked_to_packed_bb(1, gr.GR_MSB_FIRST)
+        self.mod = digital.generic_mod(constellation, samples_per_symbol=sps, excess_bw=ebw)
+        self.connect( self, self.repack, self.mod, self )
+
+
+class transmitter_bpsk(transmitter_mpsk):
     modname = "BPSK"
     def __init__(self):
-        transmitter_mapper.__init__(self, mapper.BPSK,
-            [0,1], "transmitter_bpsk", sps, ebw)
+        transmitter_mpsk.__init__(self, 2, self.modname)
 
-class transmitter_qpsk(transmitter_mapper):
+class transmitter_qpsk(transmitter_mpsk):
     modname = "QPSK"
     def __init__(self):
-        transmitter_mapper.__init__(self, mapper.QPSK,
-            [0,1,3,2], "transmitter_qpsk", sps, ebw)
+        transmitter_mpsk.__init__(self, 4, self.modname)
 
-class transmitter_8psk(transmitter_mapper):
+class transmitter_8psk(transmitter_mpsk):
     modname = "8PSK"
     def __init__(self):
-        transmitter_mapper.__init__(self, mapper.PSK8,
-            [0,1,3,2,7,6,4,5], "transmitter_8psk", sps, ebw)
+        transmitter_mpsk.__init__(self, 8, self.modname)
 
-class transmitter_pam4(transmitter_mapper):
-    modname = "PAM4"
-    def __init__(self):
-        transmitter_mapper.__init__(self, mapper.PAM4,
-            [0,1,3,2], "transmitter_pam4", sps, ebw)
-
-class transmitter_qam16(transmitter_mapper):
+class transmitter_qam16(transmitter_mqam):
     modname = "QAM16"
     def __init__(self):
-        transmitter_mapper.__init__(self, mapper.QAM16,
-            [2,6,14,10,3,7,15,11,1,5,13,9,0,4,12,8], "transmitter_qam16", sps, ebw)
+        transmitter_mqam.__init__(self, 16, self.modname)
 
-class transmitter_qam64(transmitter_mapper):
+class transmitter_qam64(transmitter_mqam):
     modname = "QAM64"
     def __init__(self):
-        transmitter_mapper.__init__(self, mapper.QAM64,
-            [0,32,8,40,3,35,11,43,
-             48,16,56,24,51,19,59,27,
-            12,44,4,36,15,47,7,39,
-            60,28,52,20,63,31,55,23,
-            2,34,10,42,1,33,9,41,
-            50,18,58,26,49,17,57,25,
-            14,46,6,38,13,45,5,37,
-            62,30,54,22,61,29,53,21], "transmitter_qam64", sps, ebw)
+        transmitter_mqam.__init__(self, 64, self.modname)
+
+# class transmitter_bpsk(gr.hier_block2):
+    # modname = "BPSK"
+    # def __init__(self):
+        # gr.hier_block2.__init__(self, "transmitter_bpsk",
+            # gr.io_signature(1, 1, gr.sizeof_char),
+            # gr.io_signature(1, 1, gr.sizeof_gr_complex))
+        # self.repack = blocks.unpacked_to_packed_bb(1, gr.GR_MSB_FIRST)
+        # constellation = digital.constellation_bpsk
+        # self.mod = digital.bpsk_mod(samples_per_symbol=sps, excess_bw=ebw)
+        # self.connect( self, self.repack, self.mod, self )
+
+# class transmitter_qpsk(transmitter_mapper):
+    # modname = "QPSK"
+    # def __init__(self):
+        # transmitter_mapper.__init__(self, mapper.QPSK,
+            # [0,1,3,2], "transmitter_qpsk", sps, ebw)
+
+# class transmitter_8psk(transmitter_mapper):
+    # modname = "8PSK"
+    # def __init__(self):
+        # transmitter_mapper.__init__(self, mapper.PSK8,
+            # [0,1,3,2,7,6,4,5], "transmitter_8psk", sps, ebw)
+
+# class transmitter_pam4(transmitter_mapper):
+    # modname = "PAM4"
+    # def __init__(self):
+        # transmitter_mapper.__init__(self, mapper.PAM4,
+            # [0,1,3,2], "transmitter_pam4", sps, ebw)
+
+# class transmitter_qam16(transmitter_mapper):
+    # modname = "QAM16"
+    # def __init__(self):
+        # transmitter_mapper.__init__(self, mapper.QAM16,
+            # [2,6,14,10,3,7,15,11,1,5,13,9,0,4,12,8], "transmitter_qam16", sps, ebw)
+
+# class transmitter_qam64(transmitter_mapper):
+    # modname = "QAM64"
+    # def __init__(self):
+        # transmitter_mapper.__init__(self, mapper.QAM64,
+            # [0,32,8,40,3,35,11,43,
+             # 48,16,56,24,51,19,59,27,
+            # 12,44,4,36,15,47,7,39,
+            # 60,28,52,20,63,31,55,23,
+            # 2,34,10,42,1,33,9,41,
+            # 50,18,58,26,49,17,57,25,
+            # 14,46,6,38,13,45,5,37,
+            # 62,30,54,22,61,29,53,21], "transmitter_qam64", sps, ebw)
 
 class transmitter_gfsk(gr.hier_block2):
     modname = "GFSK"
@@ -138,8 +196,12 @@ class transmitter_amssb(gr.hier_block2):
         self.connect( self, self.interp, self.mul, self.add, self.mod, self.filt, self )
         self.connect( self.src, (self.mod,1) )
 
+# transmitters = {
+    # "discrete":[transmitter_bpsk, transmitter_qpsk, transmitter_8psk, transmitter_pam4, transmitter_qam16, transmitter_qam64, transmitter_gfsk, transmitter_cpfsk],
+    # "continuous":[transmitter_fm, transmitter_am, transmitter_amssb]
+    # }
 
 transmitters = {
-    "discrete":[transmitter_bpsk, transmitter_qpsk, transmitter_8psk, transmitter_pam4, transmitter_qam16, transmitter_qam64, transmitter_gfsk, transmitter_cpfsk],
-    "continuous":[transmitter_fm, transmitter_am, transmitter_amssb]
+        "discrete":[transmitter_bpsk, transmitter_qpsk, transmitter_8psk, transmitter_qam16, transmitter_qam64, transmitter_gfsk, transmitter_cpfsk],
+        # "continuous":[transmitter_fm, transmitter_am, transmitter_amssb]
     }
